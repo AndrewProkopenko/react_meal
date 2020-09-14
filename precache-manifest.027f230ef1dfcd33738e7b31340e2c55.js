123c073c72beba09b208df8baf3973e452b17bb1
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2020-09-14 20:22:13 +0300

    Updates

---
+++ precache-manifest.027f230ef1dfcd33738e7b31340e2c55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5dbdb15556f0c14310167c086441d9",
+    "url": "/react_meal/index.html"
+  },
+  {
+    "revision": "52f9f316d8ab4455a4f9",
+    "url": "/react_meal/static/css/main.e876147e.chunk.css"
+  },
+  {
+    "revision": "18b2e651246a8c172f5f",
+    "url": "/react_meal/static/js/2.42850249.chunk.js"
+  },
+  {
+    "revision": "329be32b7d5199e4dc4e1dda122faca8",
+    "url": "/react_meal/static/js/2.42850249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f9f316d8ab4455a4f9",
+    "url": "/react_meal/static/js/main.c6e6ef79.chunk.js"
+  },
+  {
+    "revision": "762f8d0397b11bb9f028",
+    "url": "/react_meal/static/js/runtime-main.1ad974c8.js"
+  }
+]);
